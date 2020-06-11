b86645c0988cf6f07ccc9314a127b7b18d53c6ce
Daegal Prayoga <33486679+daegalp@users.noreply.github.com>
2020-06-11 11:52:52 +0700

    Create Simpanan.java

---
+++ src/main/java/com/kopwan/model/entity/Simpanan.java
@@ -0,0 +1,24 @@
+package com.kopwan.model.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.kopwan.model.response.anggota.AnggotaResponse;
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Document(collection = Simpanan.COLLECTION_NAME)
+public class Simpanan extends BaseEntity{
+    public static final String COLLECTION_NAME = "SIMPANAN";
+    private AnggotaResponse anggota;
+    private String type;
+    private int nominal;
+    private String month;
+    private int year;
+}
