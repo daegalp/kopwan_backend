41dbb96643581676f223149174d11b2642bf8e58
Daegal Prayoga <33486679+daegalp@users.noreply.github.com>
2020-06-11 10:46:40 +0700

    Create AnggotaRequest.java

---
+++ src/main/java/com/kopwan/model/request/AnggotaRequest.java
@@ -0,0 +1,20 @@
+package com.kopwan.model.request;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
+public class AnggotaRequest {
+    private String no;
+    private String name;
+    private int rw;
+}
