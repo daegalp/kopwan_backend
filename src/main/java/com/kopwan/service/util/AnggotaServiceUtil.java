3c5e45235f2f939385164769d681f93a5ca442f0
Daegal Prayoga <33486679+daegalp@users.noreply.github.com>
2020-06-11 10:46:49 +0700

    Create AnggotaServiceUtil.java

---
+++ src/main/java/com/kopwan/service/util/AnggotaServiceUtil.java
@@ -0,0 +1,26 @@
+package com.kopwan.service.util;
+
+import com.kopwan.model.Anggota;
+import com.kopwan.model.request.AnggotaRequest;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnggotaServiceUtil {
+
+    public Anggota convertToAnggota(AnggotaRequest request){
+        Anggota anggota = new Anggota();
+        BeanUtils.copyProperties(request, anggota);
+        return anggota;
+    }
+
+    public Anggota copyRequest(AnggotaRequest request, Anggota result){
+        BeanUtils.copyProperties(request, result);
+        return result;
+    }
+
+    public Anggota delete(Anggota anggota){
+        anggota.setMarkForDelete(true);
+        return anggota;
+    }
+}
