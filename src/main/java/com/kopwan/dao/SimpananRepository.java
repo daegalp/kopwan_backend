5495072583a79be7ce5b20403b40a77e6b228d4f
Daegal Prayoga <33486679+daegalp@users.noreply.github.com>
2020-06-11 13:46:06 +0700

    Update SimpananRepository.java

--- src/main/java/com/kopwan/dao/SimpananRepository.java
+++ src/main/java/com/kopwan/dao/SimpananRepository.java
@@ -1,11 +1,13 @@
 package com.kopwan.dao;
 
 import com.kopwan.model.entity.Simpanan;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Repository
-public interface SimpananRepository extends ReactiveMongoRepository<Simpanan, String> {
+public interface SimpananRepository extends ReactiveMongoRepository<Simpanan, String>, SimpananRepositoryCustom {
     Mono<Simpanan> findByIdAndMarkForDeleteFalse(String id);
 }
