701e6bb44c42ccc6c4f9e3d893a34d9a43b88103
Daegal Prayoga <33486679+daegalp@users.noreply.github.com>
2020-06-11 11:53:08 +0700

    Create SimpananService.java

---
+++ src/main/java/com/kopwan/service/SimpananService.java
@@ -0,0 +1,29 @@
+package com.kopwan.service;
+
+import com.kopwan.dao.SimpananRepository;
+import com.kopwan.model.entity.Simpanan;
+import com.kopwan.model.enums.ErrorCode;
+import com.kopwan.model.exception.DataNotFoundException;
+import com.kopwan.model.request.SimpananRequest;
+import com.kopwan.service.util.SimpananServiceUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class SimpananService {
+    @Autowired
+    private SimpananRepository simpananRepository;
+    @Autowired
+    private SimpananServiceUtil util;
+
+    public Mono<Void> createSimpanan(SimpananRequest request){
+        return simpananRepository.save(util.convertToSimpanan(request))
+                .then();
+    }
+
+    public Mono<Simpanan> getDetailSimpanan(String id){
+        return simpananRepository.findByIdAndMarkForDeleteFalse(id)
+                .switchIfEmpty(Mono.error(new DataNotFoundException(ErrorCode.SIMPANAN_NOT_FOUND)));
+    }
+}
