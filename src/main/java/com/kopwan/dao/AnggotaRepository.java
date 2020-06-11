33fe1fcdce8396932c15aeecddaeeea13923e7bf
Daegal Prayoga <33486679+daegalp@users.noreply.github.com>
2020-06-11 10:46:30 +0700

    Update AnggotaRepository.java

--- src/main/java/com/kopwan/dao/AnggotaRepository.java
+++ src/main/java/com/kopwan/dao/AnggotaRepository.java
@@ -1,6 +1,7 @@
 package com.kopwan.dao;
 
 import com.kopwan.model.Anggota;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
@@ -9,5 +10,8 @@ import reactor.core.publisher.Mono;
 @Repository
 public interface AnggotaRepository extends ReactiveMongoRepository<Anggota, String> {
     Mono<Anggota> findByNameAndMarkForDeleteFalse(String name);
-    Flux<Anggota> findAllByMarkForDeleteFalse();
+    Mono<Anggota> findByNoAndMarkForDeleteFalse(String no);
+    Flux<Anggota> findAllByMarkForDeleteFalseOrderByRw(Pageable pageable);
+
+    Mono<Long> countByMarkForDeleteFalse();
 }
