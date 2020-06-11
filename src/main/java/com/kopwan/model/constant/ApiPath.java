df76d67f48a09f56937bbd0833b0da512fb62ef4
Daegal Prayoga <33486679+daegalp@users.noreply.github.com>
2020-06-11 10:46:35 +0700

    Update ApiPath.java

--- src/main/java/com/kopwan/model/constant/ApiPath.java
+++ src/main/java/com/kopwan/model/constant/ApiPath.java
@@ -1,6 +1,8 @@
 package com.kopwan.model.constant;
 
 public class ApiPath {
+    // ANGGOTA PATH
     public static final String GET_ANGGOTA_BY_NAME = "/api/anggota/{name}";
-    public static final String GET_ALL_ANGGOTA = "/api/anggota";
+    public static final String GET_ANGGOTA_BY_NO = "/api/anggota/{no}";
+    public static final String ANGGOTA = "/api/anggota";
 }
