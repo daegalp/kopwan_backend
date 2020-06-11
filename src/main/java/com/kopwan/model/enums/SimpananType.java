1b725da54e49ae14851795c0816fc328b106bdd0
Daegal Prayoga <33486679+daegalp@users.noreply.github.com>
2020-06-11 11:52:58 +0700

    Create SimpananType.java

---
+++ src/main/java/com/kopwan/model/enums/SimpananType.java
@@ -0,0 +1,27 @@
+package com.kopwan.model.enums;
+
+import com.kopwan.model.exception.DataNotFoundException;
+
+public enum SimpananType {
+    POKOK("POKOK"),
+    WAJIB("WAJIB"),
+    SUKARELA("SUKARELA");
+
+    private final String description;
+
+    SimpananType(String description) {
+        this.description = description;
+    }
+
+    public static SimpananType validateType(String simpananType) {
+        try {
+            return SimpananType.valueOf(simpananType.toUpperCase());
+        } catch (Exception e) {
+            throw new DataNotFoundException(ErrorCode.INVALID_SIMPANAN_TYPE);
+        }
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
