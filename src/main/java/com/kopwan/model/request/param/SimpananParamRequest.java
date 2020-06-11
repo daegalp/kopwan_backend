e8d9d5c570255ee81111625644160b17505660d8
Daegal Prayoga <33486679+daegalp@users.noreply.github.com>
2020-06-11 13:46:10 +0700

    Create SimpananParamRequest.java

---
+++ src/main/java/com/kopwan/model/request/param/SimpananParamRequest.java
@@ -0,0 +1,26 @@
+package com.kopwan.model.request.param;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
+public class SimpananParamRequest {
+    private int page;
+    private int size;
+
+    private int rw;
+    private String type;
+    private String month;
+    private int year;
+}
