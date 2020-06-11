be4f5165fbf2ed5207048a214f3abc3da71b3487
Daegal Prayoga <33486679+daegalp@users.noreply.github.com>
2020-06-11 11:53:06 +0700

    Create SimpananDetailResponse.java

---
+++ src/main/java/com/kopwan/model/response/simpanan/SimpananDetailResponse.java
@@ -0,0 +1,24 @@
+package com.kopwan.model.response.simpanan;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.kopwan.model.response.anggota.AnggotaResponse;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
+public class SimpananDetailResponse {
+    private String id;
+    private AnggotaResponse anggota;
+    private String type;
+    private int nominal;
+    private String month;
+    private int year;
+}
