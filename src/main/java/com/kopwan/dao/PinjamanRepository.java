99b5f28d0e79577385bf0ae3e0fa35d5bbb38a06
Daegal Prayoga <33486679+daegalp@users.noreply.github.com>
2020-06-12 10:19:04 +0700

    Create PinjamanRepository.java

---
+++ src/main/java/com/kopwan/dao/PinjamanRepository.java
@@ -0,0 +1,12 @@
+package com.kopwan.dao;
+
+import com.kopwan.model.entity.Pinjaman;
+import com.kopwan.model.response.anggota.AnggotaResponse;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
+
+@Repository
+public interface PinjamanRepository extends ReactiveMongoRepository<Pinjaman, String> {
+    Mono<Pinjaman> findByAnggotaAndLunasFalseAndMarkForDeleteFalse(AnggotaResponse anggota);
+}
