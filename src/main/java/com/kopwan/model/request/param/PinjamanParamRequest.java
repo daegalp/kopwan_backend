fb91dac4f91673014d0f5be46aa847fa9e526a7a
Daegal Prayoga <33486679+daegalp@users.noreply.github.com>
2020-06-14 10:26:51 +0700

    Create PinjamanParamRequest.java

---
+++ src/main/java/com/kopwan/model/request/param/PinjamanParamRequest.java
@@ -0,0 +1,25 @@
+package com.kopwan.model.request.param;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
+public class PinjamanParamRequest {
+    private int page;
+    private int size;
+
+    private int rw;
+    private int no;
+    private String name;
+    private String month;
+    private int year;
+}
