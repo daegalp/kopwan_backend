852385f17784ba59cfca3536320507baa8d46e1b
Daegal Prayoga <33486679+daegalp@users.noreply.github.com>
2020-06-14 11:36:56 +0700

    Update SimpananRequest.java

--- src/main/java/com/kopwan/model/request/SimpananRequest.java
+++ src/main/java/com/kopwan/model/request/SimpananRequest.java
@@ -2,7 +2,6 @@ package com.kopwan.model.request;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.kopwan.model.entity.Anggota;
 import lombok.*;
 
 @Data
@@ -12,7 +11,7 @@ import lombok.*;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class SimpananRequest {
-    private Anggota anggota;
+    private String no;
     private String type;
     private int nominal;
     private String month;
