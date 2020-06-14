a64ac9088ad66de08a4e955b48f9f39a077277d8
Daegal Prayoga <33486679+daegalp@users.noreply.github.com>
2020-06-14 10:48:47 +0700

    Update CicilanPinjamanService.java

--- src/main/java/com/kopwan/service/CicilanPinjamanService.java
+++ src/main/java/com/kopwan/service/CicilanPinjamanService.java
@@ -2,7 +2,10 @@ package com.kopwan.service;
 
 import com.kopwan.dao.CicilanPinjamanRepository;
 import com.kopwan.model.entity.CicilanPinjaman;
+import com.kopwan.model.enums.ErrorCode;
+import com.kopwan.model.exception.DataNotFoundException;
 import com.kopwan.model.request.param.CicilanPinjamanParam;
+import com.kopwan.service.util.CicilanPinjamanServiceUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
@@ -12,8 +15,17 @@ import reactor.core.publisher.Mono;
 public class CicilanPinjamanService {
     @Autowired
     private CicilanPinjamanRepository repository;
+    @Autowired
+    private CicilanPinjamanServiceUtil util;
 
     public Mono<Page<CicilanPinjaman>> filterCicilan(CicilanPinjamanParam request){
         return repository.filterCicilanPinjaman(request);
+    }
+
+    public Mono<Void> deleteCicilanPinjaman(String id) {
+        return repository.findByIdAndMarkForDeleteFalse(id)
+                .switchIfEmpty(Mono.error(new DataNotFoundException(ErrorCode.CICILAN_NOT_FOUND)))
+                .flatMap(result -> repository.save(util.delete(result)))
+                .then();
     }
 }
