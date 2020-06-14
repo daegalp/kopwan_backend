12ae972a977d888b18af15ed7101e248221eb07d
Daegal Prayoga <33486679+daegalp@users.noreply.github.com>
2020-06-14 10:27:01 +0700

    Update SimpananRepositoryImpl.java

--- src/main/java/com/kopwan/dao/impl/SimpananRepositoryImpl.java
+++ src/main/java/com/kopwan/dao/impl/SimpananRepositoryImpl.java
@@ -1,12 +1,12 @@
 package com.kopwan.dao.impl;
 
 import com.kopwan.dao.SimpananRepositoryCustom;
+import com.kopwan.dao.util.RepositoryUtil;
 import com.kopwan.model.entity.Simpanan;
 import com.kopwan.model.request.param.SimpananParamRequest;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.*;
-import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
@@ -15,7 +15,7 @@ import reactor.core.publisher.Mono;
 @Repository
 public class SimpananRepositoryImpl implements SimpananRepositoryCustom {
     @Autowired
-    private ReactiveMongoTemplate reactiveMongoTemplate;
+    private RepositoryUtil repositoryUtil;
 
     @Override
     public Mono<Page<Simpanan>> filterSimpanan(SimpananParamRequest request) {
@@ -40,6 +40,10 @@ public class SimpananRepositoryImpl implements SimpananRepositoryCustom {
             query.addCriteria(Criteria.where("anggota.no")
                     .is(request.getNo()));
         }
+        if (StringUtils.isNotBlank(request.getName())) {
+            query.addCriteria(Criteria.where("anggota.name")
+                    .regex(request.getName(), "i"));
+        }
         if(request.getYear() != 999){
             query.addCriteria(Criteria.where("year")
                     .is(request.getYear()));
@@ -47,19 +51,6 @@ public class SimpananRepositoryImpl implements SimpananRepositoryCustom {
         if (StringUtils.isNotBlank(request.getMonth()) || request.getYear() != 999) {
             query.with(Sort.by(Sort.Direction.ASC, "anggota.rw"));
         }
-        return findAllPageable(query, Simpanan.class, pageRequest);
-    }
-
-    public <T> Mono<Page<T>> findAllPageable(Query query, Class<T> entity, PageRequest pageRequest) {
-        return reactiveMongoTemplate.count(query, entity)
-                .flatMap(count -> findAll(count, pageRequest, query, entity));
-    }
-
-    private <T> Mono<Page<T>> findAll(Long totalRecords, Pageable pageable,
-                                      Query query, Class<T> entity) {
-        query.with(pageable);
-        return reactiveMongoTemplate.find(query, entity)
-                .collectList()
-                .map(data -> new PageImpl<>(data, pageable, totalRecords));
+        return repositoryUtil.findAllPageable(query, Simpanan.class, pageRequest);
     }
 }
