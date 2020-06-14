9d81a9e40e691e6490f36a1e3d7fc5d2a0c88057
Daegal Prayoga <33486679+daegalp@users.noreply.github.com>
2020-06-14 11:22:49 +0700

    Create CicilanRequest.java

---
+++ src/main/java/com/kopwan/model/request/CicilanRequest.java
@@ -0,0 +1,20 @@
+package com.kopwan.model.request;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
+public class CicilanRequest {
+    private String no;
+    private String month;
+    private int year;
+}
