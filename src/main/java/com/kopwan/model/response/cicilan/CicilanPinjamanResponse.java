5edf6e33d9253084ae902582bf74e4bb02fa9956
Daegal Prayoga <33486679+daegalp@users.noreply.github.com>
2020-06-14 10:43:59 +0700

    Create CicilanPinjamanResponse.java

---
+++ src/main/java/com/kopwan/model/response/cicilan/CicilanPinjamanResponse.java
@@ -0,0 +1,24 @@
+package com.kopwan.model.response.cicilan;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.kopwan.model.response.anggota.AnggotaResponse;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
+public class CicilanPinjamanResponse {
+    private String id;
+    private AnggotaResponse anggota;
+    private int pokok;
+    private int jasa;
+    private String month;
+    private int year;
+}
