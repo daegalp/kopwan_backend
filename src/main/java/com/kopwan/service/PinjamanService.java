90f36223117b41e6c2b8066c9cbf6fb3c34b8312
Daegal Prayoga <33486679+daegalp@users.noreply.github.com>
2020-06-14 10:27:09 +0700

    Update PinjamanService.java

--- src/main/java/com/kopwan/service/PinjamanService.java
+++ src/main/java/com/kopwan/service/PinjamanService.java
@@ -6,8 +6,10 @@ import com.kopwan.model.enums.ErrorCode;
 import com.kopwan.model.exception.DataNotFoundException;
 import com.kopwan.model.exception.ValidationException;
 import com.kopwan.model.request.PinjamanRequest;
+import com.kopwan.model.request.param.PinjamanParamRequest;
 import com.kopwan.service.util.PinjamanServiceUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
@@ -23,6 +25,10 @@ public class PinjamanService {
                 .flatMap(data -> Mono.error(new ValidationException(ErrorCode.ANGGOTA_STILL_HAS_PINJAMAN)))
                 .switchIfEmpty(pinjamanRepository.save(util.convertToPinjaman(request)))
                 .then();
+    }
+
+    public Mono<Page<Pinjaman>> filterPinjaman(PinjamanParamRequest request){
+        return pinjamanRepository.filterPinjaman(request);
     }
 
     public Mono<Pinjaman> updatePinjaman(String id, PinjamanRequest request) {
