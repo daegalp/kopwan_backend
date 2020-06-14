0fabfc70c92f730cf54c8471e1b4516ec505d373
Daegal Prayoga <33486679+daegalp@users.noreply.github.com>
2020-06-14 10:48:39 +0700

    Update CicilanPinjamanController.java

--- src/main/java/com/kopwan/controller/CicilanPinjamanController.java
+++ src/main/java/com/kopwan/controller/CicilanPinjamanController.java
@@ -3,14 +3,13 @@ package com.kopwan.controller;
 import com.kopwan.model.constant.ApiPath;
 import com.kopwan.model.entity.CicilanPinjaman;
 import com.kopwan.model.request.param.CicilanPinjamanParam;
+import com.kopwan.model.response.RestBaseResponse;
 import com.kopwan.model.response.RestListResponse;
 import com.kopwan.model.response.cicilan.CicilanPinjamanResponse;
 import com.kopwan.service.CicilanPinjamanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
@@ -35,6 +34,15 @@ public class CicilanPinjamanController extends BaseController {
         return service.filterCicilan(createParamRequest(page, size, no, name, month, year, rw))
                 .map(data -> toListResponse(createResponseList(data.getContent()),
                         buildPageMetaData(page, size, data.getTotalElements())))
+                .doOnError(this::handleError)
+                .subscribeOn(Schedulers.elastic());
+    }
+
+    @DeleteMapping(value = ApiPath.CICILAN_BY_ID,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<RestBaseResponse> deleteCicilanPinjaman(@PathVariable String id) {
+        return service.deleteCicilanPinjaman(id)
+                .thenReturn(toBaseResponse())
                 .doOnError(this::handleError)
                 .subscribeOn(Schedulers.elastic());
     }
