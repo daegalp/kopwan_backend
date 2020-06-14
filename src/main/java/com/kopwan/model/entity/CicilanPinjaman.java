a5e9d360f5e2196631de258585e8ce399d6d20a8
Daegal Prayoga <33486679+daegalp@users.noreply.github.com>
2020-06-14 10:43:54 +0700

    Create CicilanPinjaman.java

---
+++ src/main/java/com/kopwan/model/entity/CicilanPinjaman.java
@@ -0,0 +1,24 @@
+package com.kopwan.model.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.kopwan.model.response.anggota.AnggotaResponse;
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@EqualsAndHashCode(callSuper = true)
+@Document(collection = CicilanPinjaman.COLLECTION_NAME)
+public class CicilanPinjaman extends BaseEntity {
+    public static final String COLLECTION_NAME = "CICILAN_PINJAMAN";
+    private AnggotaResponse anggota;
+    private int pokok;
+    private int jasa;
+    private String month;
+    private int year;
+}
