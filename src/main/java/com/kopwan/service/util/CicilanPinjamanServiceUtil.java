44e26781a31d3e078674b94341e8964e3aac3d5c
Daegal Prayoga <33486679+daegalp@users.noreply.github.com>
2020-06-14 11:23:02 +0700

    Update CicilanPinjamanServiceUtil.java

--- src/main/java/com/kopwan/service/util/CicilanPinjamanServiceUtil.java
+++ src/main/java/com/kopwan/service/util/CicilanPinjamanServiceUtil.java
@@ -1,13 +1,33 @@
 package com.kopwan.service.util;
 
 import com.kopwan.model.entity.CicilanPinjaman;
+import com.kopwan.model.entity.Pinjaman;
+import com.kopwan.model.request.CicilanRequest;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CicilanPinjamanServiceUtil {
 
+    public CicilanPinjaman convertToCicilan(CicilanRequest request, Pinjaman pinjaman) {
+        return CicilanPinjaman.builder()
+                .anggota(pinjaman.getAnggota())
+                .pokok(getValuePokok(pinjaman.getNominal(), pinjaman.getTarget()))
+                .jasa(getValueJasa(pinjaman.getNominal()))
+                .month(request.getMonth())
+                .year(request.getYear())
+                .build();
+    }
+
     public CicilanPinjaman delete(CicilanPinjaman cicilanPinjaman) {
         cicilanPinjaman.setMarkForDelete(true);
         return cicilanPinjaman;
+    }
+
+    private int getValuePokok(int nominal, int target) {
+        return nominal / target;
+    }
+
+    private int getValueJasa(int nominal) {
+        return nominal * 5 / 100;
     }
 }
