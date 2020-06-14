53d93db96007201ab9a3fa207675a96a78ec5ff2
Daegal Prayoga <33486679+daegalp@users.noreply.github.com>
2020-06-14 10:48:41 +0700

    Update CicilanPinjamanRepository.java

--- src/main/java/com/kopwan/dao/CicilanPinjamanRepository.java
+++ src/main/java/com/kopwan/dao/CicilanPinjamanRepository.java
@@ -3,8 +3,10 @@ package com.kopwan.dao;
 import com.kopwan.model.entity.CicilanPinjaman;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface CicilanPinjamanRepository
         extends ReactiveMongoRepository<CicilanPinjaman, String>, CicilanPinjamanRepositoryCustom {
+    Mono<CicilanPinjaman> findByIdAndMarkForDeleteFalse(String id);
 }
