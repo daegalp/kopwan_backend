232dca37ed9d41f29106bf0ce8b77da2ee87d443
Daegal Prayoga <33486679+daegalp@users.noreply.github.com>
2020-06-14 11:22:58 +0700

    Update CicilanPinjamanService.java

--- src/main/java/com/kopwan/service/CicilanPinjamanService.java
+++ src/main/java/com/kopwan/service/CicilanPinjamanService.java
@@ -4,6 +4,7 @@ import com.kopwan.dao.CicilanPinjamanRepository;
 import com.kopwan.model.entity.CicilanPinjaman;
 import com.kopwan.model.enums.ErrorCode;
 import com.kopwan.model.exception.DataNotFoundException;
+import com.kopwan.model.request.CicilanRequest;
 import com.kopwan.model.request.param.CicilanPinjamanParam;
 import com.kopwan.service.util.CicilanPinjamanServiceUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,9 +18,18 @@ public class CicilanPinjamanService {
     private CicilanPinjamanRepository repository;
     @Autowired
     private CicilanPinjamanServiceUtil util;
+    @Autowired
+    private PinjamanService pinjamanService;
 
     public Mono<Page<CicilanPinjaman>> filterCicilan(CicilanPinjamanParam request){
         return repository.filterCicilanPinjaman(request);
+    }
+
+    public Mono<Void> createCicilan(CicilanRequest request) {
+        return pinjamanService.findByNoAnggota(request.getNo())
+                .flatMap(result -> repository.save(util.convertToCicilan(request, result)))
+                .flatMap(result -> pinjamanService.updateActual(request.getNo()))
+                .then();
     }
 
     public Mono<Void> deleteCicilanPinjaman(String id) {
