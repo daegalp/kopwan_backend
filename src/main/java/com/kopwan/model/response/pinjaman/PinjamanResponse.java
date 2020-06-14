9c822ba23e4be569d2c24c9643a8ffa54ac77c74
Daegal Prayoga <33486679+daegalp@users.noreply.github.com>
2020-06-14 10:07:38 +0700

    Create PinjamanResponse.java

---
+++ src/main/java/com/kopwan/model/response/pinjaman/PinjamanResponse.java
@@ -0,0 +1,26 @@
+package com.kopwan.model.response.pinjaman;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.kopwan.model.response.anggota.AnggotaResponse;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
+public class PinjamanResponse {
+    private String id;
+    private AnggotaResponse anggota;
+    private int nominal;
+    private String month;
+    private int year;
+    private boolean lunas;
+    private int actual;
+    private int target;
+}
