2da247abd0f8bab342929d37525be771d1d4cbcf
Daegal Prayoga <33486679+daegalp@users.noreply.github.com>
2020-06-15 12:51:05 +0700

    Create KasRequest.java

---
+++ src/main/java/com/kopwan/model/request/KasRequest.java
@@ -0,0 +1,23 @@
+package com.kopwan.model.request;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
+public class KasRequest {
+    private String name;
+    private String type;
+    private String alurKas;
+    private int nominal;
+    private int month;
+    private int year;
+}
