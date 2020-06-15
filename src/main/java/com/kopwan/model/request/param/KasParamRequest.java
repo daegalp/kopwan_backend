47cab5b80e83ac59dc868202b1ddc9f432c7394c
Daegal Prayoga <33486679+daegalp@users.noreply.github.com>
2020-06-15 13:02:40 +0700

    Create KasParamRequest.java

---
+++ src/main/java/com/kopwan/model/request/param/KasParamRequest.java
@@ -0,0 +1,19 @@
+package com.kopwan.model.request.param;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
+public class KasParamRequest {
+    private int month;
+    private int year;
+}
