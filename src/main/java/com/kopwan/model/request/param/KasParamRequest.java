6222a45b224d44eb2d13fe8675eee4ab89d11567
Daegal Prayoga <33486679+daegalp@users.noreply.github.com>
2020-06-16 18:58:10 +0700

    Update KasParamRequest.java

--- src/main/java/com/kopwan/model/request/param/KasParamRequest.java
+++ src/main/java/com/kopwan/model/request/param/KasParamRequest.java
@@ -14,9 +14,6 @@ import lombok.NoArgsConstructor;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Builder
 public class KasParamRequest {
-    private int page;
-    private int size;
-
     private int month;
     private int year;
 }
