af57c55583fb71d60a6a3cbead50a8153f22ff5a
Daegal Prayoga <33486679+daegalp@users.noreply.github.com>
2020-06-16 20:11:48 +0700

    Create DebitKredit.java

---
+++ src/main/java/com/kopwan/model/response/neraca/DebitKredit.java
@@ -0,0 +1,19 @@
+package com.kopwan.model.response.neraca;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
+public class DebitKredit {
+    private int debit;
+    private int kredit;
+}
