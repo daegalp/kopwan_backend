92df0e285f24b2ea393ff2fec6639e4d166f9d74
Daegal Prayoga <33486679+daegalp@users.noreply.github.com>
2020-06-16 10:28:23 +0700

    Create TotalKasResponse.java

---
+++ src/main/java/com/kopwan/model/response/kas/TotalKasResponse.java
@@ -0,0 +1,19 @@
+package com.kopwan.model.response.kas;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
+public class TotalKasResponse {
+    private int kasMasuk;
+    private int kasKeluar;
+}
