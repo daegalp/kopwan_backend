f8167160892601fe63843186238add9075ad16e7
Daegal Prayoga <33486679+daegalp@users.noreply.github.com>
2020-06-16 10:08:46 +0700

    Update KasRepository.java

--- src/main/java/com/kopwan/dao/KasRepository.java
+++ src/main/java/com/kopwan/dao/KasRepository.java
@@ -1,11 +1,16 @@
 package com.kopwan.dao;
 
 import com.kopwan.model.entity.Kas;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Repository
 public interface KasRepository extends ReactiveMongoRepository<Kas, String> {
     Mono<Kas> findByIdAndMarkForDeleteFalse(String id);
+    Flux<Kas> findAllByMonthAndYearAndMarkForDeleteFalse(int month, int year, Pageable pageable);
+
+    Mono<Long> countAllByMonthAndYearAndMarkForDeleteFalse(int month, int year);
 }
